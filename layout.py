ee3bcfd6339267ecf252c0b0836f32140b5a8293
Erwan <easyastro@enviephoto.fr>
2023-09-11 17:54:52 +0200

    doc + gui

--- layout.py
+++ layout.py
@@ -5,7 +5,7 @@ def get_layout():
 
     return [
         [sg.Text('Sélectionnez un répertoire contenant des fichiers SER :')],
-        [sg.InputText(key='-FOLDER-'), sg.FolderBrowse()],
+        [sg.InputText(key='-FOLDER-',size=(70,200)), sg.FolderBrowse()],
         [sg.Checkbox('Include subdirectory', key='-SUBDIR-',default=False),sg.Checkbox('Delete originals', key='-DELETE-',default=False)],
         [sg.Text('Frames to keep (percent or number)'), sg.InputText(key="number",default_text='200')],
         [sg.Radio('Percentage', "TYPE", key='-OPTION_PERCENTAGE-', default=False, enable_events=True),sg.Radio('Number', "TYPE", key='-OPTION_NUMBER-', default=True,  enable_events=True)],
